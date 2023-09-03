ce4e00c8f482929392c02fd1004d82ba89c33205
skchqhdpdy <aniren@debian.moe>
2023-09-04 01:46:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ OSUAPIKEY = conf.config["osu"]["APIKEY"]
 db = pymysql.connect(host=host, user=user, password=password, db=table, charset="utf8")
 cur = db.cursor()
 
-url = ["https://api.chimu.moe/cheesegull/s/", "https://storage.ripple.moe/api/s/", "https://osu.direct/api/s/", "https://redstar.moe/api/v1/get_beatmaps?s="]
+url = ["https://api.chimu.moe/cheesegull/s/", "https://storage.ripple.moe/api/s/", "https://osu.direct/api/s/", "https://redstar.moe/api/v1/get_beatmaps?s=", f"https://osu.ppy.sh/api/get_beatmaps?k={OSUAPIKEY}&s="]
 
 bid = input("bid 입력 : ")
 
@@ -77,9 +77,90 @@ def get_redstarAPI_ChildrenBeatmaps(bsid):
     if r.status_code == 200:
         r = r.json()
 
+    r2 = requests.get(f"{url[4]}{bsid}")
+    if r2.status_code == 200:
+        r2 = r2.json()
+
+    r3 = requests.get(f"{url[0]}{bsid}")
+    if r3.status_code == 200:
+        r3 = r3.json()["ChildrenBeatmaps"]
+
+    def b(txt, bid):
+        if " cs" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' cs')]
+            for i in r2:
+                if i["version"] == bName:
+                    return {"CS": 0, "HP": i["diff_drain"], "playcount": i["playcount"], "passcount": i["passcount"]}
+
+        if " ar" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' ar')]
+            for i in r2:
+                if i["version"] == bName:
+                    return {"CS": i["diff_size"], "HP": i["diff_drain"], "AR": 0, "playcount": i["playcount"], "passcount": i["passcount"]}
+
+        if " hp" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' hp')]
+            for i in r2:
+                if i["version"] == bName:
+                    return {"CS": i["diff_size"], "HP": 0, "playcount": i["playcount"], "Passcount": i["passcount"]}
+
+        if " od" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' od')]
+            for i in r2:
+                if i["version"] == bName:
+                    return {"CS": i["diff_size"], "HP": i["diff_drain"], "OD": 0, "playcount": i["playcount"], "Passcount": i["passcount"]}
+                
+        for i in r2:
+            if i["version"] == txt:
+                return {"CS": i["diff_size"], "HP": i["diff_drain"], "playcount": i["playcount"], "passcount": i["passcount"]}
+
+    def c(txt, bid):
+        if " cs" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' cs')]
+            for i in r3:
+                if i["DiffName"] == bName:
+                    return {"CS": 0, "HP": i["HP"], "Playcount": i["Playcount"], "Passcount": i["Passcount"]}
+
+        if " ar" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' ar')]
+            for i in r3:
+                if i["DiffName"] == bName:
+                    return {"CS": i["CS"], "HP": i["HP"], "AR": 0, "Playcount": i["Playcount"], "Passcount": i["Passcount"]}
+
+        if " hp" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' hp')]
+            for i in r3:
+                if i["DiffName"] == bName:
+                    return {"CS": i["CS"], "HP": 0, "Playcount": i["Playcount"], "Passcount": i["Passcount"]}
+
+        if " od" in txt.lower() and int(bid) < 0:
+            bName = txt[:txt.lower().find(' od')]
+            for i in r3:
+                if i["DiffName"] == bName:
+                    return {"CS": i["CS"], "HP": i["HP"], "OD": 0, "Playcount": i["Playcount"], "Passcount": i["Passcount"]}
+
+        for i in r3:
+            #RedstarAPI
+            #48498 masterpiece 예외처리 시발련아
+            if int(bid) == -14:
+                if i["AR"] == 8:
+                   return {"CS": i["CS"], "HP": i["HP"], "Playcount": i["Playcount"], "Passcount": i["Passcount"]}
+            #RedstarAPI
+            #10000000 Black + White (TV Size) 예외처리 시발련아
+            elif int(bid) == -19:
+                return {"CS": -1, "HP": 7, "Playcount": -1, "Passcount": -1}
+
+            elif i["DiffName"] == txt:
+                return {"CS": i["CS"], "HP": i["HP"], "Playcount": i["Playcount"], "Passcount": i["Passcount"]}
+            
+
     Dict = []
 
     for i in r:
+        if (len(r) != len(r2)) or (len(r) != len(r3)):
+            bancho = b(i["version"], i["beatmap_id"])
+            chimu = c(i["version"], i["beatmap_id"])
+
         Dict2 = {}
         Dict2["ParentSetID"] = i["beatmapset_id"]
         Dict2["BeatmapID"] = i["beatmap_id"]
@@ -87,22 +168,41 @@ def get_redstarAPI_ChildrenBeatmaps(bsid):
         Dict2["HitLength"] = i["hit_length"]
         Dict2["DiffName"] = i["version"]
         Dict2["FileMD5"] = i["file_md5"]
-        Dict2["CS"] = -1
+        try:
+            Dict2["CS"] = bancho["diff_size"]
+        except:
+            log.warning("child | CS | 2차 Bancho, 3차 chimu 까지옴")
+            Dict2["CS"] = chimu["CS"]
+
         Dict2["AR"] = i["diff_approach"]
-        Dict2["HP"] = -1
+        try:
+            Dict2["HP"] = bancho["diff_drain"]
+        except:
+            log.warning("child | HP | 2차 Bancho, 3차 chimu 까지옴")
+            Dict2["HP"] = chimu["HP"]
+
         Dict2["OD"] = i["diff_overall"]
         Dict2["Mode"] = i["mode"]
         Dict2["BPM"] = i["bpm"]
-        Dict2["Playcount"] = -1 #i["playcount"]
-        Dict2["Passcount"] = -1 #i["passcount"]
+        try:
+            Dict2["Playcount"] = bancho["playcount"]
+        except:
+            log.warning("child | Playcount | 2차 Bancho, 3차 chimu 까지옴")
+            Dict2["Playcount"] = chimu["Playcount"]
+
+        try:
+            Dict2["Passcount"] = bancho["passcount"]
+        except:
+            log.warning("child | Passcount | 2차 Bancho, 3차 chimu 까지옴")
+            Dict2["Passcount"] = chimu["Passcount"]
+
         Dict2["MaxCombo"] = i["max_combo"]
         Dict2["DifficultyRating"] = i["difficultyrating"]
 
         Dict2["RankedStatus"] = i["approved"]
         Dict2["ApprovedDate"] = i["approved_date"]
-
+        print("")
         Dict.append(Dict2)
-    
     return Dict
 
 def rq_cheesegull():
@@ -117,6 +217,22 @@ def rq_cheesegull():
             break
         else:
             log.error(f"Failed {urlRq}")
+
+    r2 = requests.get(f"{url[4]}{bsid}")
+    if r2.status_code == 200:
+        r2 = r2.json()
+
+    r3 = requests.get(f"{url[0]}{bsid}")
+    if r3.status_code == 200:
+        r3 = r3.json()
+
+    #RedstarAPI
+    #10000000 Black + White (TV Size) 예외처리 시발련아 22
+    if int(bid) == -19:
+        r3 = {"Creator": "arpia97", "Source": "Mondaiji-tachi ga Isekai kara Kuru Sou Desu yo?", \
+              "Tags": "momochi dakedekaane xinely hinsvar kuro usagi bunny girl asuka you izayoi adaption novel opening osutrainer", \
+              "HasVideo": 0, "Genre": -1, "Language": -3, "Favourites": -1
+              }
 
     print("")
 
@@ -171,37 +287,67 @@ def rq_cheesegull():
     try:
         Dict["Creator"] = r["Creator"]
     except:
-        Dict["Creator"] = -1 #r[0]["creator"]
+        try:
+            Dict["Creator"] = r2[0]["creator"]
+        except:
+            log.warning("Creator | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["Creator"] = r3["Creator"]
 
     try:    
         Dict["Source"] = r["Source"]
     except:
-        Dict["Source"] = -1 #r[0]["source"]
+        try:
+            Dict["Source"] = r2[0]["source"]
+        except:
+            log.warning("Source | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["Source"] = r3["Source"]
 
     try:
         Dict["Tags"] = r["Tags"]
     except:
-        Dict["Tags"] = -1 #r[0]["tags"]
+        try:
+            Dict["Tags"] = r2[0]["tags"] + "osutrainer"
+        except:
+            log.warning("Tags | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["Tags"] = r3["Tags"] + "osutrainer"
 
     try:
         Dict["HasVideo"] = r["HasVideo"]
     except:
-        Dict["HasVideo"] = 0
+        try:
+            Dict["HasVideo"] = r2[0]["video"]
+        except:
+            log.warning("HasVideo | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["HasVideo"] = r3["HasVideo"]
 
     try:
         Dict["Genre"] = r["Genre"]
     except:
-        Dict["Genre"] = -1 #r[0]["genre_id"]
+        try:
+            Dict["Genre"] = r2[0]["genre_id"]
+        except:
+            log.warning("Genre | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["Genre"] = r3["Genre"]
 
     try:
         Dict["Language"] = r["Language"]
     except:
-        Dict["Language"] = -1 #r[0]["language_id"]
+        try:
+            Dict["Language"] = r2[0]["language_id"]
+        except:
+            log.warning("Language | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["Language"] = r3["Language"]
 
     try:
         Dict["Favourites"] = r["Favourites"]
     except:
-        Dict["Favourites"] = -1 #r[0]["favourite_count"]
+        try:
+            Dict["Favourites"] = r2[0]["favourite_count"]
+        except:
+            log.warning("Language | 1차 Redstar, 2차 Bancho, 3차 chimu 까지옴")
+            Dict["Favourites"] = r3["Favourites"]
+
+    print("")
 
     try:        
         num = []
